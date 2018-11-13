84b9aa22ed32ff0a2eefcb9ace177f641f3aaee6
Thomas Forrer <thomas.forrer@wuerth-phoenix.com>
2018-11-13 16:32:01 +0100

    adding some formatting functions to Point and Polygon

--- src/esri/src/esrilib/polygon.rs
+++ src/esri/src/esrilib/polygon.rs
@@ -8,5 +8,25 @@ impl Polygon {
     pub fn new(points: Vec<Point>) -> Polygon {
         Polygon { points }
     }
+
+    pub fn to_string(&self) -> String {
+        let mut s: String = format!("[ {:?} ]", self.points);
+        s.push_str("a test00");
+        s
+    }
 }
 
+
+impl std::fmt::Debug for Polygon {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "[ {:?} ]", self.points)
+    }
+}
+
+impl std::string::ToString for Polygon {
+    fn to_string(&self) -> String {
+        let mut s: String = format!("[ {:?} ]", self.points);
+        s.push_str("a test00");
+        s
+    }
+}
--- src/utils/src/main.rs
+++ src/utils/src/main.rs
@@ -3,5 +3,5 @@ extern crate esrilib;
 fn main() {
 
     let esri = esrilib::esrilib::Esri::new_from_file();
-    println!("Hello {}!", esri.to_string());
+    println!("Hello {:?}!", esri.to_polygons());
 }
